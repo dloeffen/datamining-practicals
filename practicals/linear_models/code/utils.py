4215d24fcfd6a754f00f998f9767bfb93ba51d89
Dirk Loeffen <d.w.m.loeffen@student.tue.nl>
2020-09-21 14:29:06 +0200

    Implemented exercise Some metrics

---
+++ practicals/linear_models/code/utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 14:09:10 2020
+
+@author: dirk
+"""
+
+def sum_squared_errors(y1, y2):
+    # Implement sum of squared errors
+    pass
+
+def mean_squared_error(y1,y2):
+    # Implement mean squared error
+    pass
+
+def root_mean_squared_error(y1,y2):
+    # Implement root squared error
+    pass
