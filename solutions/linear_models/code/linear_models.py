fcf6f90089c34d4ea7d1baafbcc394ebb81f922d
Dirk Loeffen <d.w.m.loeffen@student.tue.nl>
2020-09-21 17:01:44 +0200

    Added exercise linear model fit

---
+++ solutions/linear_models/code/linear_models.py
@@ -0,0 +1,10 @@
+# Author: Dirk W.M. Loeffen
+
+def lse_fit(X,y):
+    # Implement a least square error fit
+    pass
+
+def lse_predict(X, theta):
+    # Implement a prediction based on the weights
+    # generated using lse_fit
+    pass
