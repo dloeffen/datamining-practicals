ef2dc5a7ea463b7e5d110dccc37d5d04dc652086
Dirk Loeffen <d.w.m.loeffen@student.tue.nl>
2020-09-21 13:09:45 +0200

    Implemented euclidian distance exercise and k means exercise

---
+++ practicals/code/kmeans.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 12:53:41 2020
+
+@author: dirk
+"""
+
+def fit(X, k, alpha):
+    # Set some samples as original means
+    
+    # Assign the samples in X to a class, use your implementation of
+    # euclidian distance in utils.py
+    
+    # recalculate the means and reassign the classes
+    
+    # check how much the means change, if it is less 
+    pass
+
+def predict(X,mu):
+    # calculate the distance form samples to different mu and assign class
+    
+    pass
