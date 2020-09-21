1535adc532c22997a08053a180c22b96838b1d03
Dirk Loeffen <d.w.m.loeffen@student.tue.nl>
2020-09-21 13:35:51 +0200

    Changed the directory layout

---
+++ solutions/clustering/code/utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:55:53 2020
+
+@author: dirk
+"""
+import numpy as np
+
+def euclidian_distance(x,y):
+    # Implement the euclidian distance using simple matrix operations
+    pass
+
+def random_float(min_value, max_value):
+    return (max_value - min_value)*np.random.random() + min_value
