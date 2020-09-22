005202b6a041019f65a5df18256f7a8b4002031c
Dirk Loeffen <d.w.m.loeffen@student.tue.nl>
2020-09-22 20:58:39 +0200

    Update exercise least squared error fit and added solutions

--- practicals/linear_models/code/utils.py
+++ practicals/linear_models/code/utils.py
@@ -5,15 +5,16 @@ Created on Mon Sep 21 14:09:10 2020
 
 @author: dirk
 """
+import numpy as np
 
 def sum_squared_errors(y1, y2):
     # Implement sum of squared errors
     pass
 
-def mean_squared_error(y1,y2):
+def mean_squared_error(y1, y2):
     # Implement mean squared error
     pass
 
-def root_mean_squared_error(y1,y2):
+def root_mean_squared_error(y1, y2):
     # Implement root squared error
     pass
--- solutions/linear_models/code/utils.py
+++ solutions/linear_models/code/utils.py
@@ -5,15 +5,19 @@ Created on Mon Sep 21 14:09:10 2020
 
 @author: dirk
 """
+import numpy as np
 
 def sum_squared_errors(y1, y2):
     # Implement sum of squared errors
-    pass
+    diff = y1 - y2
+    square = np.multiply(diff, diff)
+    
+    return np.sum(square)
 
-def mean_squared_error(y1,y2):
+def mean_squared_error(y1, y2):
     # Implement mean squared error
-    pass
+    return sum_squared_errors(y1,y2)/len(y1)
 
-def root_mean_squared_error(y1,y2):
+def root_mean_squared_error(y1, y2):
     # Implement root squared error
-    pass
+    return np.sqrt(mean_squared_error(y1,y2))
