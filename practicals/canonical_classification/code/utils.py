e2dd118457dab0c84964558cfe93dcbd6d43282d
Dirk Loeffen <d.w.m.loeffen@student.tue.nl>
2020-09-25 10:51:24 +0200

    Added exercise naive bayes with solutions

---
+++ practicals/canonical_classification/code/utils.py
@@ -0,0 +1,15 @@
+# Author: Dirk W.M. Loeffen
+# file containing different utilities that are
+# useful for this exercise
+
+import numpy as np
+
+def probability_binary(x):
+    # implement a probability measure for a binary value
+    
+    pass
+
+def probability_histogram(x, max_value, bin_width):
+    # implement probability histogram
+    
+    pass
